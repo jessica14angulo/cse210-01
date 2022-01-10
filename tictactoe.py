83f794f0913c2bb014e98d9ea7cf200bb80f7294
jessica14angulo <ang21016@byui.edu>
2022-01-10 17:44:47 -0400

    winner def added

--- tictactoe.py
+++ tictactoe.py
@@ -12,3 +12,13 @@ def show_board(board):
     print("_+_+_")
     print(f"{board[6]}|{board[7]}|{board[8]}")
     print()
+
+def winner(board):
+    return (board[0] == board[1] == board[2] or
+            board[3] == board[4] == board[5] or
+            board[6] == board[7] == board[8] or
+            board[0] == board[3] == board[6] or
+            board[1] == board[4] == board[7] or
+            board[2] == board[5] == board[8] or
+            board[0] == board[4] == board[8] or
+            board[2] == board[4] == board[6]) 
