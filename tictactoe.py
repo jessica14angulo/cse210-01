ecf55ee5a74c6cb9c1dd57c39613ed3d1aaaae3d
jessica14angulo <ang21016@byui.edu>
2022-01-10 17:44:08 -0400

    show board def added

--- tictactoe.py
+++ tictactoe.py
@@ -3,3 +3,12 @@ def create_board():
     for square in range(9):
         board.append(square +1)
     return board
+
+def show_board(board):
+    print()
+    print(f"{board[0]}|{board[1]}|{board[2]}")
+    print("_+_+_")
+    print(f"{board[3]}|{board[4]}|{board[5]}")
+    print("_+_+_")
+    print(f"{board[6]}|{board[7]}|{board[8]}")
+    print()
